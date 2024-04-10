f3e2ebe5fc20083c55f587dcff79e44c299b24ba
Sergio29o03 <sergio29@DESKTOP-MMLHIU2.localdomain>
2024-04-10 06:09:19 -0500

    seconde commit

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,7 +1,15 @@
-class DevelopmentConfig():
+class Config:
+    SECRET_KEY = 'B!1w8NAt1T^%kvhUI*S^'
+
+
+class DevelopmentConfig(Config):
     DEBUG = True
-    
+    MYSQL_HOST = '192.168.1.104'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = '123456'
+    MYSQL_DB = 'proyectoanalisis'
+
 
 config = {
-    'development' : DevelopmentConfig
+    'development': DevelopmentConfig
 }
